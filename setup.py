09271e73b6d1894bc66908c7c6056ca6a4941609
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2020-07-08 13:21:51 -0700

    Change how setup and requirements are managed

--- setup.py
+++ setup.py
@@ -1,9 +1,30 @@
 #!/usr/bin/env python
 
 from os.path import exists
-from setuptools import setup, find_packages
+
+from setuptools import find_packages, setup
 
 import versioneer
+
+requirements = [
+    "aiobotocore>=0.10.2",
+    "dask>=2.2.0",
+    "distributed>=2.3.1",
+    "azureml-sdk>=1.0.83",
+]
+
+gcp_requirements = [
+    "google-api-python-client>=1.9.3",
+    "google-auth-oauthlib>=0.4.1",
+]
+
+extra_requirements = {
+    "gcp": gcp_requirements,
+    "all": [
+        *requirements,
+        *gcp_requirements,
+    ]
+}
 
 setup(
     name="dask-cloudprovider",
@@ -17,7 +38,8 @@ setup(
     include_package_data=True,
     long_description=(open("README.md").read() if exists("README.md") else ""),
     zip_safe=False,
-    install_requires=list(open("requirements.txt").read().strip().split("\n")),
+    install_requires=requirements,
+    extras_require=extra_requirements,
     entry_points="""
     [console_scripts]
     dask-ecs=dask_cloudprovider.cli.ecs:go
